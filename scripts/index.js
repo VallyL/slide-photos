104c08221c2fc62262faba772df6bbe621decc53
VallyL <levin.valeriia01@gmail.com>
2024-07-10 09:39:02 +0200

    added elements

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,21 @@ const images = [
   "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Altdorfer-Donau.jpg/220px-Altdorfer-Donau.jpg",
   "https://images.ctfassets.net/hrltx12pl8hq/a2hkMAaruSQ8haQZ4rBL9/8ff4a6f289b9ca3f4e6474f29793a74a/nature-image-for-website.jpg?fit=fill&w=600&h=400",
 ];
+
+const frame = document.createElement("div");
+frame.classList.add("frame");
+const cards = document.createElement("div");
+cards.classList.add("cards");
+const triggers = document.createElement("div");
+triggers.classList.add("triggers");
+
+const leftBtn = document.createElement("button");
+leftBtn.classList.add("leftBtn");
+leftBtn.textContent = "<";
+const rightBtn = document.createElement("button");
+rightBtn.classList.add("rightBtn");
+rightBtn.textContent = ">";
+
+triggers.append(leftBtn, rightBtn);
+frame.append(cards, triggers);
+root.append(frame);
